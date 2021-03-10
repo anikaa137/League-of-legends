08d40072e590dcf9f19ba958bf7ed1aa7454ef27
anika137 <anikaa137@gmail.com>
2021-03-10 12:01:57 +0600

    set nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,41 @@
 import React from "react";
 import './Header.css'
-import backgroundImg from '../../images/resize.png'
+import backgroundImg from '../../images/resize.png';
+import navIcon from '../../images/backgroundImg (3).png'
 const Header = () => {
   return (
     <div>
       <div className="background-img">
-      <img src={backgroundImg} class="img-fluid" alt="..."></img>
+        <img src={backgroundImg} class="img-fluid" alt="..."></img>
       </div>
-   
+
       <div>
-          <h1>dsflk</h1>
+      <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <br/>
+      <img src={navIcon} alt=""/>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
       </div>
     </div>
   );
