f45d21839f36018d1dcdc281b931ca2b14df1730
anika137 <anikaa137@gmail.com>
2021-03-11 14:17:30 +0600

    solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import LeagueDetails from "./components/LeagueDetails/LeagueDetails";
 import Notfound from "./components/NotFound/Notfound";
